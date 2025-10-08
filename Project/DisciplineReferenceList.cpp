94783bff1c0d91eb367568e06ef39333f8ddb66d
VolchM <volchkovmisha@gmail.com>
2025-10-08 21:49:04 +0700

    Added DisciplineReferenceList class

---
+++ Project/DisciplineReferenceList.cpp
@@ -0,0 +1,46 @@
+#include "DisciplineReferenceList.hpp"
+
+
+DisciplineReferenceList::DisciplineReferenceList(): m_disciplines() {}
+
+DisciplineReferenceList::DisciplineReferenceList(const std::vector<Discipline*>& disciplines): m_disciplines(disciplines) {}
+
+
+int DisciplineReferenceList::GetSize() const {
+	return m_disciplines.size();
+}
+
+Discipline* DisciplineReferenceList::GetDisciplineAt(int index) const {
+	return m_disciplines[index];
+}
+
+int DisciplineReferenceList::FindDiscipline(Discipline* discipline) const {
+	for (int i = 0; i < m_disciplines.size(); i++) {
+		if (m_disciplines[i]->Equals(*discipline)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+const std::vector<Discipline*>& DisciplineReferenceList::GetDisciplines() const {
+	return m_disciplines;
+}
+
+
+void DisciplineReferenceList::AddDiscipline(Discipline* discipline) {
+	if (this->FindDiscipline(discipline) == -1) {
+		m_disciplines.push_back(discipline);
+	}
+}
+
+void DisciplineReferenceList::RemoveDisciplineAt(int index) {
+	m_disciplines.erase(m_disciplines.begin() + index);
+}
+
+void DisciplineReferenceList::RemoveDiscipline(Discipline* discipline) {
+	int index = this->FindDiscipline(discipline);
+	if (index != -1) {
+		this->RemoveDisciplineAt(index);
+	}
+}
