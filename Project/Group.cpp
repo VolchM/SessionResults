f464e42fbe785f6000c262138b905d83f46784e2
VolchM <volchkovmisha@gmail.com>
2025-10-09 17:34:30 +0700

    Added Group class

---
+++ Project/Group.cpp
@@ -0,0 +1,86 @@
+#include "Group.hpp"
+
+#include "StringUtils.hpp"
+
+#include <stdexcept>
+
+
+Group::Group(Speciality* speciality, const std::string& name, int course) {
+    if (speciality == nullptr) {
+        throw std::invalid_argument("Speciality cannot be nullptr");
+    }
+    m_speciality = speciality;
+    this->SetName(name);
+    this->SetCourse(course);
+}
+
+Group::~Group() {
+    for (Student* student : m_students) {
+        delete student;
+    }
+}
+
+const std::string& Group::GetName() const {
+    return m_name;
+}
+
+void Group::SetName(const std::string& value) {
+    std::string newValue = StringUtils::Trim(value);
+    if (newValue.size() == 0) {
+        throw std::invalid_argument("Group name cannot be empty");
+    }
+    m_name = std::move(newValue);
+}
+
+int Group::GetCourse() const {
+    return m_course;
+}
+
+void Group::SetCourse(int value) {
+    if (!(value >= 1 && value <= 4)) {
+        throw std::invalid_argument("Course must be a number between 1 and 4");
+    }
+}
+
+Speciality* Group::GetSpeciality() const {
+    return m_speciality;
+}
+
+int Group::GetStudentCount() {
+    return m_students.size();
+}
+
+Student* Group::GetStudentAt(int index) const {
+    return m_students[index];
+}
+
+const std::vector<Student*>& Group::GetStudents() const {
+    return m_students;
+}
+
+int Group::FindStudentByID(unsigned int studentID) const {
+    for (int i = 0; i < m_students.size(); i++) {
+        if (m_students[i]->GetStudentID() == studentID) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void Group::AddStudent(Student* student) {
+    if (this->FindStudentByID(student->GetStudentID()) == -1) {
+        m_students.push_back(student);
+    }
+}
+
+void Group::DeleteStudentByID(unsigned int studentID) {
+    int index = this->FindStudentByID(studentID);
+    if (index != -1) {
+        delete m_students[index];
+        m_students.erase(m_students.begin() + index);
+    }
+}
+
+void Group::DeleteStudent(Student* student) {
+    this->DeleteStudentByID(student->GetStudentID());
+}
---
+++ Project/Group.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Student.hpp"
+
+#include <string>
+#include <vector>
+
+
+class Speciality;
+
+// Группа
+class Group {
+private:
+	std::string m_name; // Название группы
+	int m_course; // Курс обучения
+	Speciality* m_speciality; // Специальность группы
+	std::vector<Student*> m_students; // Cписок студентов
+
+public:
+	Group(Speciality* speciality, const std::string& name, int course);
+	~Group();
+
+
+	// Возвращает название группы
+	const std::string& GetName() const;
+
+	// Устанавливает название группы
+	void SetName(const std::string& value);
+
+	// Возвращает курс обучения
+	int GetCourse() const;
+
+	// Устанавливает курс обучения
+	void SetCourse(int value);
+
+	// Возвращает специальность группы
+	Speciality* GetSpeciality() const;
+
+	// Возвращает количество студентов в группе
+	int GetStudentCount();
+
+	// Возвращает студента по указанному индексу
+	Student* GetStudentAt(int index) const;
+
+	// Возврашает список студентов
+	const std::vector<Student*>& GetStudents() const;
+
+	// Находит студента по номеру студенческго билета и возвращает его индекс. Если такого студента нет в списке возвращает -1
+	int FindStudentByID(unsigned int studentID) const;
+
+
+	// Добавляет студента
+	void AddStudent(Student* student);
+
+	// Удаляет студента по номеру студенческого билета
+	void DeleteStudentByID(unsigned int studentID);
+
+	// Удаляет студента
+	void DeleteStudent(Student* student);
+};
