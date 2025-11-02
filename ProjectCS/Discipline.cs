b1cb885526a0f74b08d358434d25c745af8f511a
VolchM <volchkovmisha@gmail.com>
2025-11-02 14:30:39 +0700

    Added Discipline class

---
+++ ProjectCS/Discipline.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionResults
+{
+    // Перечисление типов аттестации
+    public enum AttestationType
+    {
+        Exam,           // Экзамен
+        PassFailExam    // Зачёт
+    }
+
+    public class Discipline
+    {
+        private string _name;
+
+
+        // Название дисциплины
+        public string Name {
+            get { return _name; }
+            set
+            {
+                string newName = value.Trim();
+                if (newName.Length == 0)
+                {
+                    throw new ArgumentException("Discipline name cannot be empty");
+                }
+                _name = newName;
+            }
+        }
+
+        // Тип аттестации
+        public AttestationType AttestationType { get; set; }
+
+
+        public Discipline(string name, AttestationType attestationType)
+        {
+            Name = name;
+            AttestationType = attestationType;
+        }
+
+
+        // Проверяет равенство с другой дисциплин
+        public override bool Equals(object? obj)
+        {
+            Discipline? other = obj as Discipline;
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Name == other.Name && AttestationType == other.AttestationType;
+            }
+        }
+    }
+}
