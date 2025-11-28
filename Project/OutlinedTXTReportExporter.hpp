a2318194d6a6cdf220e5a6d9d2108b72d8196db5
VolchM <volchkovmisha@gmail.com>
2025-11-28 19:50:01 +0700

    Added OutlinedTXTReportExporter class

---
+++ Project/OutlinedTXTReportExporter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "TXTReportExporter.hpp"
+
+class OutlinedTXTReportExporter : public TXTReportExporter {
+public:
+	OutlinedTXTReportExporter(const std::string& filePath, const std::string& title = "", const std::string& body = "", bool includeDate = false);
+	OutlinedTXTReportExporter(const OutlinedTXTReportExporter& other);
+
+	std::string RenderTable(const GroupTableData& data) override;
+};
+
