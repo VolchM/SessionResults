5e4effe59c5a24538a4e3907e8884d00d29b75b9
VolchM <volchkovmisha@gmail.com>
2025-12-25 20:32:02 +0700

    Added Faculty class

---
+++ ProjectCS/Faculty.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SessionResultsCS
+{
+    public class Faculty
+    {
+        public const int SPECIALITY_NOT_FOUND = -1;
+
+
+        private string _name;
+        // Список специальностей
+        private List<Speciality> _specialities;
+
+
+        // Название факультета
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                string newName = value.Trim();
+                if (newName.Length == 0)
+                {
+                    throw new ArgumentException("Faculty name cannot be empty");
+                }
+                _name = newName;
+            }
+        }
+
+
+        public Faculty(string name)
+        {
+            Name = name;
+            _specialities = new List<Speciality>();
+        }
+
+
+        // Возвращает количество специальностей
+        public int GetSpecialityCount()
+        {
+            return _specialities.Count;
+        }
+
+        // Возвращает специальность по указанному индексу
+        public Speciality GetSpecialityAt(int index)
+        {
+            return _specialities[index];
+        }
+
+        // Возвращает список специальностей
+        public ReadOnlyCollection<Speciality> GetSpecialities()
+        {
+            return _specialities.AsReadOnly();
+        }
+
+        // Находит специальность по названию и возвращает её индекс. Если такой специальности нет в списке возвращает SPECIALITY_NOT_FOUND
+        public int FindSpecialityByName(string name)
+        {
+            return _specialities.FindIndex(speciality => speciality.Name == name);
+        }
+
+        // Добавляет специальность
+        public void AddSpeciality(Speciality speciality)
+        {
+            if (FindSpecialityByName(speciality.Name) != SPECIALITY_NOT_FOUND)
+            {
+                throw new ArgumentException("Faculty already contains added speciality");
+            }
+            _specialities.Add(speciality);
+        }
+
+        // Удаляет специальность по названию
+        public void DeleteSpecialityByName(string name)
+        {
+            int index = FindSpecialityByName(name);
+            if (index == SPECIALITY_NOT_FOUND)
+            {
+                throw new ArgumentException("Faculty doesn't contain deleted speciality");
+            }
+            _specialities.RemoveAt(index);
+        }
+
+        // Удаляет специальность
+        public void DeleteSpeciality(Speciality speciality)
+        {
+            DeleteSpecialityByName(speciality.Name);
+        }
+    }
+}
