760ec88068e2c827ee8d13baab55bb65c6ecb352
VolchM <volchkovmisha@gmail.com>
2026-01-12 15:46:51 +0700

    Added ItemPicker class

---
+++ ProjectCS/UI/ItemPicker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SessionResultsCS.UI
+{
+    public class ItemPicker : BaseItemPicker
+    {
+        public ItemPicker(IEnumerable<string> items)
+            : base(items) { }
+
+
+        protected override int GetInput()
+        {
+            while (true)
+            {
+                Console.Write("Введите номер: ");
+                string input = Console.ReadLine()!;
+                PrintedLines++;
+
+                if (int.TryParse(input, out int value) && (value >= 1 && value <= ItemCount))
+                {
+                    return value - 1;
+                }
+            }
+        }
+    }
+}
