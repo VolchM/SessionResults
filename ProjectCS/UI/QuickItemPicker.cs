36e0eec7454ad412820694bf9f62e33a06d3d8c7
VolchM <volchkovmisha@gmail.com>
2026-01-12 16:03:21 +0700

    Added QuickItemPicker class

---
+++ ProjectCS/UI/QuickItemPicker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SessionResultsCS.UI
+{
+    public class QuickItemPicker : BaseItemPicker
+    {
+        public QuickItemPicker(IEnumerable<string> items) 
+            : base(items)
+        {
+            if (ItemCount > 9)
+            {
+                throw new ArgumentException("Maximum number of items is 9");
+            }
+        }
+
+        protected override int GetInput()
+        {
+            Console.Write("Нажмите номер: ");
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+
+                if (char.IsAsciiDigit(input))
+                {
+                    int value = (int)char.GetNumericValue(input);
+
+                    if (value >= 1 && value <= ItemCount)
+                    {
+                        return value - 1;
+                    }
+                }
+            }
+        }
+    }
+}
