a60c37915151d5e58651560ae3df9536b55af0d9
VolchM <volchkovmisha@gmail.com>
2026-01-12 15:23:04 +0700

    Added IPage and LoopPage classes

---
+++ ProjectCS/UI/IPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionResultsCS.UI
+{
+    public interface IPage
+    {
+        public void Start();
+    }
+}
---
+++ ProjectCS/UI/LoopPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionResultsCS.UI
+{
+    public abstract class LoopPage: IPage
+    {
+        private bool _looping;
+
+        public void Start()
+        {
+            _looping = true;
+            while (_looping)
+            {
+                Loop();
+            }
+        }
+
+        protected abstract void Loop();
+
+        protected void StopLoop()
+        {
+            _looping = false;
+        }
+    }
+}
