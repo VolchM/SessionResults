fdddde5c79ac61c1eacfd3ed3b1373c6c35bf0a9
VolchM <volchkovmisha@gmail.com>
2026-01-12 16:24:44 +0700

    Added MenuActionPicker class

---
+++ ProjectCS/UI/MenuActionPicker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SessionResultsCS.UI
+{
+    public class MenuActionPicker
+    {
+        private QuickItemPicker _picker;
+        private IList<Action> _actions;
+
+        public MenuActionPicker(IEnumerable<string> items, IEnumerable<Action> actions)
+        {
+            _picker = new QuickItemPicker(items);
+            _actions = actions.ToList();
+            if (_picker.ItemCount != _actions.Count)
+            {
+                throw new ArgumentException("Item count is not equal to actions count");
+            }
+        }
+
+        public MenuActionPicker(IEnumerable<(string, Action)> items)
+            : this(items.Select((tuple) => tuple.Item1), items.Select((tuple) => tuple.Item2)) { }
+
+
+        public void PickAndRun()
+        {
+            int selectedIndex = _picker.Pick();
+            _actions[selectedIndex].Invoke();
+        }
+    }
+}
